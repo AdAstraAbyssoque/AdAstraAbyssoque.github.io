12eeb35ad5d599d6334b4a7c8cd58ff1f9f20cd1
Shan <bliu699@outlook.com>
2024-10-03 17:36:45 +0800

    Site updated: 2024-10-03 17:36:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/03/hello-world/","2024/10/03/音频识别-从MLP到GRU：作业引发的血案/"];function toRandomPost(){
+var posts=["2024/10/03/Hello World Again!/","2024/10/03/音频识别-从MLP到GRU：作业引发的血案/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
