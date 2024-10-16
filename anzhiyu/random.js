46103b21198887173d4c8e3a4f5279909f7e1b88
Shan <bliu699@outlook.com>
2024-10-16 20:39:43 +0800

    Site updated: 2024-10-16 20:39:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/03/Hello World Again!/","2024/10/08/Ubuntu调控CPU频率/","2024/10/04/minGRU选读/","2024/10/03/音频识别-从MLP到GRU：作业引发的血案/"];function toRandomPost(){
+var posts=["2024/10/03/Hello World Again!/","2024/10/08/Ubuntu调控CPU频率/","2024/10/04/minGRU选读/","2024/10/03/音频识别-从MLP到GRU：作业引发的血案/","2024/10/15/太虚幻梦/","2024/10/16/想去海边/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
